5455ecf7e5ef9aa9e2c334091056835303e5904d
Muhammad Ahmed Haque <118427337+M-AhmedHaque@users.noreply.github.com>
2025-06-01 18:57:07 +0000

    vars only

---
+++ 1_basics/1_variables.js
@@ -0,0 +1,7 @@
+const email = "muhammadahmedhaque@gmail.com"
+var username = "Muhammad Ahmed Haque"
+let password = "sachmainyehiha"
+id = 12312412
+// email="mahmedhaq@gmail.com" cant do
+
+console.table([id,email,username,password])
