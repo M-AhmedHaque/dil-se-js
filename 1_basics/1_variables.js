787e98c3bc81415d232e7ddd92e114f2f326e062
Muhammad Ahmed Haque <118427337+M-AhmedHaque@users.noreply.github.com>
2025-06-01 19:07:29 +0000

    background of vars

--- 1_basics/1_variables.js
+++ 1_basics/1_variables.js
@@ -1,7 +1,13 @@
 const email = "muhammadahmedhaque@gmail.com"
-var username = "Muhammad Ahmed Haque"
-let password = "sachmainyehiha"
-id = 12312412
+
+//we can declare varible using 3 ways: var, let, or just name
+
+var username = "Muhammad Ahmed Haque" //never use this, it is an early js thing and it had problems with scope, if you have declare a var in a file or block then if you declare var again with that same name it will see both as same and will overwrite the value
+
+let password = "sachmainyehiha" //then to solve issue of scope let was introduced.
+
+id = 12312412 //this can also declare new variable but not recommended.
+
 // email="mahmedhaq@gmail.com" cant do
 
 console.table([id,email,username,password])
